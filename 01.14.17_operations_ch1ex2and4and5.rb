3f5d5b9c4b770da80cc7ec9014608a16434fe284
Refayat Haque <refayathaque@gmail.com>
2017-01-14 14:52:33 -0500

    an operations Ruby exercise

---
+++ 01.14.17_operations_ch1ex2and4and5.rb
@@ -0,0 +1,38 @@
+=begin Use the modulo operator, division, or a combination
+of both to take a 4 digit number and find the digit in
+the: 1) thousands place 2) hundreds place 3) tens place
+4) ones place.
+=end
+
+# Modulo (%) gives the remainder of a division operation
+
+puts "Thousands = "
+puts 1234/1000
+puts "Hundreds = "
+puts 1234%1000/100
+puts "Tens = "
+puts 1234%1000%100/10
+puts "Ones = "
+puts 1234%1000%100%10
+
+puts "----"
+
+=begin White a program that outputs the factorial of the
+numbers 5, 6, 7, and 8.
+=end
+
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+puts "----"
+
+=begin Write a program that calculates the squares of 3
+float numbers of your choosing and outputs the result to
+the screen.
+=end
+
+puts 1.234*1.234
+puts 2.345*2.345
+puts 3.4567*3.4567
