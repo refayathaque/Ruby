7ee855622fd3d1a4b2e3ce56ab6f18a7a2e05728
Refayat Haque <refayathaque@gmail.com>
2017-01-20 23:27:54 -0500

    added some comments

--- anagram_exercise.rb
+++ anagram_exercise.rb
@@ -4,13 +4,15 @@ array = ['demo', 'none', 'tied', 'evil', 'dome','mode','live',
 hash = {}
 
 array.each do |element|
-  key = element.chars.sort.join
+  key = element.chars.sort.join # Can also do element.split(//).sort.join
   if hash.has_key?(key)
-    hash[key].push(element)
+    hash[key].push(element) # Can also do hash[key]<<element
   else
     hash[key] = [element]
   end
 end
+
+# Block above POPULATES the hash
 
 hash.each do |key, value|
   p value
