c1f384a892e87ac7fa2e186a506d76f85d5dc4c4
Refayat Haque <refayathaque@gmail.com>
2017-01-22 21:26:45 -0500

    added notes related to scope

--- 01.22.17_variable_scope_ch2ex5and6.rb
+++ 01.22.17_variable_scope_ch2ex5and6.rb
@@ -30,3 +30,15 @@ puts x # Outputs error message, because `x` is initiated in
 =begin What does x print to the screen in each case? Do they
 both give errors? Are the errors different? Why?
 =end
+
+=begin I commented out the first block of code because I
+needed to demonstrate the Variable Scope Rule (Inner scope
+can access variables intitalized in an outer scope, but
+not vice versa). If the first block of remained operational
+then the second block of code would end up working (and
+there wouldn't be an error message) because in the first
+block the variable `x` is initiated in the OUTER SCOPE.
+OUTER SCOPE can access varibles initiated in the OUTER SCOPE,
+OUTER SCOPE cannot access variables initiated in the INNER
+SCOPE.
+=end
