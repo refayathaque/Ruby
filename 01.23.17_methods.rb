426d9081f5c5747b713bad52df799f628acee25a
Refayat Haque <refayathaque@gmail.com>
2017-01-23 15:07:11 -0500

    added default parameters

--- 01.23.17_methods.rb
+++ 01.23.17_methods.rb
@@ -1,4 +1,5 @@
-=begin When we have a piece of code that must be executed
+=begin
+When we have a piece of code that must be executed
 many times we can use a METHOD. We begin a METHOD first by
 defining it with the reserved word `def`, we then follow it
 with a name for the METHOD. Finally, we end our METHOD with
@@ -11,21 +12,24 @@ def says(words) # (words) is known as the PARAMETER
   puts words
 end
 
-says("Hello Refayat.") # (...) is known as the ARGUMENT
-says("You have to work very hard to learn Ruby well.")
-says("It'll take perserverance and unwavering commitment!")
+says("Hello Refayat") # (...) is known as the ARGUMENT
+says("You have to work very hard to learn Ruby well")
+says("It'll take perserverance and unwavering commitment")
 
 puts "----"
 
 # We used a METHOD above instead of doing the following:
 
-puts "Hello Refayat."
-puts "You have to work very hard to learn Ruby well."
-puts "It'll take perserverance and unwavering commitment!"
+puts "Hello Refayat"
+puts "You have to work very hard to learn Ruby well"
+puts "It'll take perserverance and unwavering commitment"
+
+puts "----"
 
 # By using a METHOD we made our program more FLEXIBLE!
 
-=begin PARAMETERs are used when we have data outside the
+=begin
+PARAMETERs are used when we have data outside the
 the METHOD's scope, and we need to give the outside data
 access to be used inside the METHOD's scope. In our METHOD
 above we needed to give the ARGUMENTs outside the METHOD's
@@ -35,9 +39,9 @@ important to give PARAMETERs meaningful and explicit names
 so it's clear to others reading our code. | ARGUMENTs are
 "passed" to a method when we call it, they are data sent
 to a method to be modified or used to return a specific
-result. In our example the first ARGUMENT is "Hello Refayat.",
+result. In our example the first ARGUMENT is "Hello Refayat",
 and it's "passed" to our METHOD 'says' when we call it.
-When we pass the string "Hello Refayat." into the METHOD
+When we pass the string "Hello Refayat" into the METHOD
 it's being assigned to the variable 'words', and we can use
 it in whatever way we want from within the METHOD. |
 *** Important here to note that the 'words' variable is
@@ -46,3 +50,51 @@ METHOD block/within the METHOD scope [it's in the inner
 scope, NOT outer]), and therefore, we cannot reference it
 outside of the 'say' METHOD.
 =end
+
+=begin
+When we call 'says("Hello Refayat")' we are passing in the
+string "Hello Refayat" as the ARGUMENT which will take place
+of the 'words' PARAMETER, then the block within the method
+is run with the 'words' variable evaluated to "Hello Refayat"
+=end
+
+=begin
+A benefit of METHODs is that it gives us the ability to make
+changes in ONE place if we need to make changes in many
+other places in our program. IE If we wanted to add a period
+(.) to the end of the strings we pass to the `say` METHOD,
+we have to make a change in only ONE place. Demonstrated below.
+=end
+
+def says(words)
+  puts words + "."
+end
+
+says("Hello Refayat")
+says("You have to work very hard to learn Ruby well")
+says("It'll take perserverance and unwavering commitment")
+
+puts "----"
+
+# DEFAULT PARAMETERs
+
+=begin
+Whenever we're defining METHODs we want to make sure that it
+always works, in case there are no ARGUMENTs being passed to
+the PARAMETER. We must have a DEFAULT PARAMETER in case the
+caller does not pass an ARGUMENT.
+=end
+
+# Let's REFACTOR our code above with a DEFAULT PARAMETER
+
+def says(words="Hello Refayat") # DEFAULT PARAMETER
+  puts words + "."
+end
+
+says()
+says("You have to work very hard to learn Ruby well")
+says("It'll take perserverance and unwavering commitment")
+
+# 'says()' prints "Hello Refayat." because we provided a
+# DEFAULT PARAMETER which is used when we call our METHOD
+# without an ARGUMENT.
