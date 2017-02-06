da8c9cb5b269c2e4ddcc731d05477bcfa3d13b3b
Refayat Haque <refayathaque@gmail.com>
2017-02-06 18:51:23 -0500

    starting loops and iterators chapter

---
+++ 02.06.17_loops_and_iterators.rb
@@ -0,0 +1,8 @@
+# LOOPS & ITERATORS
+
+=begin
+Some operations in Ruby are best served with a LOOP. What is a LOOP?
+A LOOP is the repetitive execution of a code for a specified amount
+of repeitions, or until a certain condition is met. There are 'while'
+LOOPs, 'do/while' LOOPs, and 'for' LOOPs.
+=end
