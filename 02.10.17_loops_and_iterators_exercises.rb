078669483d2219fcc4499bef4c93a38196ab837a
Refayat Haque <refayathaque@gmail.com>
2017-02-10 23:52:40 -0500

    added ex 3

--- 02.10.17_loops_and_iterators_exercises.rb
+++ 02.10.17_loops_and_iterators_exercises.rb
@@ -1,6 +1,6 @@
 =begin
-Ex. 2) Write a while loop that takes input from the user, performs an action, and only stops
-when the user types "STOP". Each loop can get info from the user.
+Ex. 2) Write a while loop that takes input from the user, performs an action, and
+only stops when the user types "STOP". Each loop can get info from the user.
 =end
 
 a = "" # Line 11 below will ASSIGN this for line 8 with the 'while conditional'
@@ -10,3 +10,18 @@ while a != "STOP" # Once this becomes 'false', the LOOP will stop. What that mea
   puts "Hey how's it going?"
   a = gets.chomp # User input, ASSIGNs variable 'a' on line 6
 end
+
+puts "----"
+
+=begin
+Ex. 3) Use the `each_with_index` method to iterate through an array of your creation
+that prints each index and value of the array.
+=end
+
+array = ["Harry", "Dick", "Tom", "Jeff", "Andrew", "George"]
+
+array.each_with_index do |name, index|
+  puts "#{index + 1}. #{name}"
+end
+
+puts "----"
