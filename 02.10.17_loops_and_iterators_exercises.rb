800a9a040aa319655bac44477a5f4f4591157805
Refayat Haque <refayathaque@gmail.com>
2017-02-11 22:44:21 -0500

    added ex 3

--- 02.10.17_loops_and_iterators_exercises.rb
+++ 02.10.17_loops_and_iterators_exercises.rb
@@ -25,3 +25,18 @@ array.each_with_index do |name, index|
 end
 
 puts "----"
+
+# Ex. 4) Write a method that counts down to zero using recursion.
+
+puts "Enter a number, then see the countdown to 0"
+userinput = gets.chomp.to_i
+
+def countdown(start)
+  puts start
+  if start > 0
+    countdown(start - 1)
+  end
+end
+
+countdown(userinput)
+puts "Lift Off!"
