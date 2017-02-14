da69c4ed9c39f549cabf57df66dd19739f46c53e
Refayat Haque <refayathaque@gmail.com>
2017-02-14 18:12:22 -0500

    working on modifying arrays

--- 02.14.17_arrays.rb
+++ 02.14.17_arrays.rb
@@ -41,4 +41,59 @@ puts array[4] # Outputs 6 then 7. Notice the difference here? [6, 7] is
               # SEPARATED with 'puts', making the output different from
               # 'p array[4]'.
 
+puts "----"
+
 # MODIFYING ARRAYs
+
+=begin
+Say we wanted to add or remove things from an ARRAY, how do we go about
+doing that? There are a couple of Array methods we can use. If we want
+to PERMANENTLY remove the last item of an array we can use Array#pop. It's
+important to note that when we call the Array#pop method we are MODIFYING
+the orginal 'array' variable, we say such methods MUTATEs THE CALLER. When
+using Array#pop what is RETURNED is the popped element (last element in the
+ARRAY).
+=end
+
+p array.pop # Output the popped element, or the last element in the ARRAY,
+            # in our case being [6, 7].
+p array # Output [1, 'Refayat', 2.345, 'Haque'], since the last element
+        # [6, 7] was PERMANENTLY removed. Array#pop MUTATEs THE CALLER.
+
+puts "----"
+
+# If we want to add [6, 7] back to the array PERMANENTLY, we can use the
+# Array#push method and send it the parameters we want to add.
+
+p array.push([6, 7]) # Output [1, 'Refayat', 2.345, 'Haque', [6, 7]]
+p array # Output [1, 'Refayat', 2.345, 'Haque', [6, 7]], we did this to
+        # check if the addition was PERMANENT, and it was, allowing us to
+        # say that Array#push is a method that MUTATEs THE CALLER.
+
+# We can also use the shovel operator (<<) to add elements. This also
+# MUTATEs THE CALLER (makes the addition PERMANENT).
+
+p array << 'Ruby' # Output [1, "Refayat", 2.345, "Haque", [6, 7], "Ruby"]
+
+puts "----"
+
+=begin
+Array#map is a method that iterates over an array and applies a block to
+each element, it then RETURNS a new array with those results. Array#collect
+is an alias to Array#map, they do the same thing. Let's see this in action.
+=end
+
+# We will square elements in an array with Array#map, then we will cube
+# elements with Array#collect.
+
+array1 = [2, 4, 6, 8]
+
+p array1.map { |element| element**2 }
+
+array1.collect do |element|
+  p element**3
+end
+
+# Why can't I get above to print out in [...] form?
+
+puts "----"
