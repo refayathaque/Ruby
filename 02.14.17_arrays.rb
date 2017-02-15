1280cb59ec57ca57ef3354403ae708a021275d73
Refayat Haque <refayathaque@gmail.com>
2017-02-14 20:49:18 -0500

    added new methods

--- 02.14.17_arrays.rb
+++ 02.14.17_arrays.rb
@@ -11,12 +11,12 @@ play around with an array that has different types of elements.
 array = [1, 'Refayat', 2.345, 'Haque', [6, 7]]
 puts array # Lists the elements vertically, without qutoations for the
            # string elements, the ARRAY element [6, 7] SEPARATED, and no
-           # [...]s. RETURNS 'nil'.
+           # [...]s. RETURNs 'nil'.
 
 puts "----"
 
 p array # Lists the elements horizontally to appear exactly like how we
-        # have it defined above (identical syntaxtually). RETURNS the same.
+        # have it defined above (identical syntaxtually). RETURNs the same.
         # [1, 'Refayat', 2.345, 'Haque', [6, 7]] -> Output AND RETURNED
 
 puts "----"
@@ -55,9 +55,9 @@ using Array#pop what is RETURNED is the popped element (last element in the
 ARRAY).
 =end
 
-p array.pop # Output the popped element, or the last element in the ARRAY,
+p array.pop # Outputs the popped element, or the last element in the ARRAY,
             # in our case being [6, 7].
-p array # Output [1, 'Refayat', 2.345, 'Haque'], since the last element
+p array # Outputs [1, 'Refayat', 2.345, 'Haque'], since the last element
         # [6, 7] was PERMANENTLY removed. Array#pop MUTATEs THE CALLER.
 
 puts "----"
@@ -65,21 +65,21 @@ puts "----"
 # If we want to add [6, 7] back to the array PERMANENTLY, we can use the
 # Array#push method and send it the parameters we want to add.
 
-p array.push([6, 7]) # Output [1, 'Refayat', 2.345, 'Haque', [6, 7]]
-p array # Output [1, 'Refayat', 2.345, 'Haque', [6, 7]], we did this to
+p array.push([6, 7]) # Outputs [1, 'Refayat', 2.345, 'Haque', [6, 7]]
+p array # Outputs [1, 'Refayat', 2.345, 'Haque', [6, 7]], we did this to
         # check if the addition was PERMANENT, and it was, allowing us to
         # say that Array#push is a method that MUTATEs THE CALLER.
 
 # We can also use the shovel operator (<<) to add elements. This also
 # MUTATEs THE CALLER (makes the addition PERMANENT).
 
-p array << 'Ruby' # Output [1, "Refayat", 2.345, "Haque", [6, 7], "Ruby"]
+p array << 'Ruby' # Outputs [1, "Refayat", 2.345, "Haque", [6, 7], "Ruby"]
 
 puts "----"
 
 =begin
 Array#map is a method that iterates over an array and applies a block to
-each element, it then RETURNS a new array with those results. Array#collect
+each element, it then RETURNs a new array with those results. Array#collect
 is an alias to Array#map, they do the same thing. Let's see this in action.
 =end
 
@@ -88,13 +88,48 @@ is an alias to Array#map, they do the same thing. Let's see this in action.
 
 array1 = [2, 4, 6, 8]
 
-p array1.map { |element| element**2 }
+p array1.map { |element| element**2 } # Outputs [4, 16, 36, 64]
 
-array1.collect do |element|
-  p element**3
-end
+p array1.collect { |element| element**3 } # Outputs [8, 64, 216, 512]
 
-# Why can't I get above to print out in [...] form? Also...doesn't MUTATEs
-# THE CALLER. 
+# These two methods DOESN'T MUTATE THE CALLER. We know this because there
+# is no change to array1, these two methods are NOT DESTRUCTIVE.
+
+puts "----"
+
+=begin
+Array#delete_at is a method used to eliminate the value at a certain index
+in ARRAYs. We should be careful with this method because it modifies an
+ARRAY DESTRUCTIVELY, in other words, it MUTATEs THE CALLER. Once we call
+this method we are changing our ARRAY PERMANENTLY. Let's see this in play.
+=end
+
+p array1.delete_at(1) # Outputs 4, value that has been deleted PERMANENTLY
+                      # Also RETURNs 4
+p array1 # Outputs [2, 6, 8], this shows that 4 was PERMANENTLY removed.
+
+puts "----"
+
+# Sometimes we'll know the value we want to delete, but not the index, in
+# such situations we will use Array#delete. Array#delete PERMANENTLY
+# deletes ALL INSTANCES of the provided value from the array. This method
+# MUTATEs THE CALLER, and is DESTRUCTIVE.
+
+array2 = [1, "Hello", 2.345, 2.345, "Ruby", [6, 7]]
+p array2.delete(2.345) # Outputs 2.345
+p array2 # Outputs [1, "Hello", "Ruby", [6, 7]] # 2.345 (both instances)
+         # were PERMANENTLY removed, and are no longer elements in array2.
+
+puts "----"
+
+# Another useful method is Array#uniq, this iterates through an ARRAY and
+# deletes duplicate values, then RETURNs the result as a new ARRAY.
+# But this method is NOT DESTRUCTIVE.
+
+array3 = [1, 2, 3, 3, 3, 4, 5, 6]
+p array3.uniq # Outputs [1, 2, 3, 4, 5, 6], we can see that two out of
+              # the three '3' elements are no longer there.
+p array3 # Outputs [1, 2, 3, 3, 3, 4, 5, 6], proving that the method is
+         # indeed NOT DESTRUCTIVE.
 
 puts "----"
