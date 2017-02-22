cced058198e9576aac8fe0a31ed26dee8866dc2d
Refayat Haque <refayathaque@gmail.com>
2017-02-22 15:55:12 -0500

    finished intro

--- 02.20.17_hashes.rb
+++ 02.20.17_hashes.rb
@@ -2,7 +2,7 @@
 
 =begin
 A HASH is a data structure which stores items by associated KEYs. Arrays,
-on the other, stores items by an ordered index. Entries in a has are
+on the other, store items by an ordered index. Entries in a HASH are
 referred to as 'key-value pairs'. HASHES are created using symbols (:)
 as keys and any data types as values. Key-value pairs in a hash are
 surrounded by curly braces '{}', and are separated with commas. HASHES
@@ -16,12 +16,12 @@ with Ruby version 1.9 does not use '=>' to separate the key and the value.
 [2] pry(main)> newsyntax = {name:'Refayat'}
 => {:name=>"Refayat"}
 
-We can have hashes with multiple key-value pairs
+We can have HASHES with multiple key-value pairs
 
 [1] pry(main)> newsyntax = {name:'Refayat', age:'27'}
 => {:name=>"Refayat", :age=>"27"}
 
-Say we want to add another key-value pair
+Say we want to add another key-value pair, how do we do that?
 
 [3] pry(main)> newsyntax[:hair] = 'black'
 => "black"
@@ -40,6 +40,36 @@ Say we wanted to retrieve a piece of information from a hash
 [7] pry(main)> newsyntax[:hair]
 => "black"
 
-# What about retrieving key using value?
+What if we wanted to retrieve the corresponding key of a specific value?
+
+# We use the Hash#key method.
+
+[11] pry(main)> newsyntax.key("Refayat")
+=> :name
+
+Say we wanted to merge two hashes together. How would we do that?
+
+# We use the Hash#merge! method if we want to mutate the caller. And
+# Hash#merge method if we dont want to mutate the caller, IE leave the
+# original hash unmodified.
+
+[12] pry(main)> newsyntax
+=> {:name=>"Refayat", :hair=>"black"}
+[13] pry(main)> newhash = {city: 'Washington DC', car: 'BMW'}
+=> {:city=>"Washington DC", :car=>"BMW"}
+[15] pry(main)> newsyntax.merge!(newhash)
+=> {:name=>"Refayat",
+ :hair=>"black",
+ :city=>"Washington DC",
+ :car=>"BMW"}
+[16] pry(main)> newsyntax
+=> {:name=>"Refayat",
+ :hair=>"black",
+ :city=>"Washington DC",
+ :car=>"BMW"}
+[17] pry(main)> newhash
+=> {:city=>"Washington DC", :car=>"BMW"}
 
 =end
+
+# ITERATING OVER HASHES
