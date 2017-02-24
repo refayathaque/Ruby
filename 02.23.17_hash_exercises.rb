aa5a73882756b7091e041c8b77040daf253983b1
Refayat Haque <refayathaque@gmail.com>
2017-02-23 23:14:38 -0500

    finished up to 3

--- 02.23.17_hash_exercises.rb
+++ 02.23.17_hash_exercises.rb
@@ -52,3 +52,46 @@ p hash1 # hash1 modified {:a=>100, :b=>250, :c=>300}
 # overwritten with the values from the other_hash (hash2 in our case).
 
 puts "----"
+
+=begin
+3. Using some of Ruby's built-in Hash methods, write a program that loops through
+a hash and prints all of the keys. Then write a program that does the same
+thing except printing the values. Finally, write a program that prints both
+=end
+
+movie_by_year = {:kill_bill_vol_1 => "2003",
+  :stewart_little => "1999",
+  :scarface => "1983",
+  :stand_up_guys => "2012",
+  :finding_dory => "2016"}
+
+movie_by_year.each do |key, value|
+  puts key
+end
+
+puts "----"
+
+movie_by_year.each do |key, value|
+  puts value
+end
+
+puts "----"
+
+movie_by_year.each do |key, value| # Hash#each
+  puts "#{key} was released in #{value}"
+end
+
+puts "----"
+
+movie_by_year.each_key { |key| puts key } # Hash#each_key
+
+puts "----"
+
+movie_by_year.each_value { |value| puts value } # Hash#each_value
+
+puts "----"
+
+movie_by_year.each_pair { |key, value| puts "#{key} was released in #{value}" }
+# Hash#each_pair
+
+puts "----"
