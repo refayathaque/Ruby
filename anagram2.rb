7c40508a850532122d5fc03020342643db6bbd20
Refayat Haque <refayathaque@gmail.com>
2017-02-24 12:28:40 -0500

    Updates

---
+++ anagram2.rb
@@ -0,0 +1,17 @@
+array = ['demo', 'none', 'tied', 'evil', 'dome','mode','live',
+  'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+  'flow', 'neon']
+hash = {}
+
+array.each do |element|
+  key = element.split(//).sort.join
+  if hash.has_key?(key)
+    hash[key].push(element)
+  else
+    hash[key] = [element]
+  end
+end
+
+hash.each do |key, value|
+  p value
+end
