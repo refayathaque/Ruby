bcf228bda4744f9aa9843b03569e204563afa0a6
Refayat Haque <refayathaque@gmail.com>
2017-03-03 21:08:00 -0500

    1

--- test.rb
+++ test.rb
@@ -1,16 +1,13 @@
-names = ['bob', 'joe', 'steve', nil, 'frank']
-
-names.each do |name|
-  begin
-    puts "#{name}'s name hash #{name.length} letters in it."
-  rescue
-    puts "Something went wrong!"
-  end
+def space_out_letters(person)
+  return person.chars.join(" ")
 end
 
-puts "----"
+def greet(person)
+  return "H e l l o, " + space_out_letters(person)
+end
 
-zero = 0
-puts "Before each call"
-zero.each { |element| puts element } rescue puts "Can't do that!"
-puts "After each call"
+def decorate_greeting(person)
+  puts greet(person)
+end
+
+decorate_greeting("John")
