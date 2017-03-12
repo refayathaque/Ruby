d2a2aa5ddeda184a93f91aa1ebe04ba05838597a
Refayat Haque <refayathaque@gmail.com>
2017-03-11 23:15:20 -0500

    Breakthrough!...maybe

--- 03.06.17_final_exercises.rb
+++ 03.06.17_final_exercises.rb
@@ -307,3 +307,19 @@ end
 p hash
 
 puts "----"
+
+# I think I finally figured this out...
+
+hashabc = {"Joe Smith"=>{}}
+
+arrayabc = ["joe@email.com", "123 Main st.", "555-123-4567"]
+
+labelarrayabc = [:email, :address, :phone]
+
+hashabc.each do |key, value|
+  labelarrayabc.each do |element|
+    hashabc["Joe Smith"][element] = arrayabc.shift
+  end
+end
+
+p hashabc
