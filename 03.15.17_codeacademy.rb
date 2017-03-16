9c1c0a7663a74625f2681a1d7b333f9f816921c4
Refayat Haque <refayathaque@gmail.com>
2017-03-15 20:58:18 -0400

    Histogram In Progress

--- 03.15.17_codeacademy.rb
+++ 03.15.17_codeacademy.rb
@@ -21,3 +21,32 @@ end
 
 # Why so much code above? It's to make sure that we get a
 # NEW LINE after every possible iteration.
+
+# Example of a HISTOGRAM
+
+puts "Please enter some text"
+text = gets.chomp
+
+words = text.split(" ") # Returns an array ['tomato', 'tomato', 'grape',
+                        # 'cherry', 'apple', 'apple', 'apple']
+
+frequencies = Hash.new(0) # 0 is the default value, we need to have the default
+                          # value here as 0, of our code below. We need the
+                          # value in the hash to start from 0 so we can increment
+                          # every time an element appears more than once.
+
+words.each do |element|
+  frequencies[element] += 1
+end
+
+p frequencies
+
+
+
+
+
+#frequencies = frequencies.sort_by {|a, b| b }
+#frequencies.reverse!
+#frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+
+# tomato tomato grape cherry apple apple apple
