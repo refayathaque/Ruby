1aac6475f29a59c08e59bfe4bfe2a8f7bde124d3
Refayat Haque <refayathaque@gmail.com>
2017-03-26 22:58:47 -0400

    .

--- anagram1.rb
+++ anagram1.rb
@@ -1,14 +1,14 @@
 array = ['demo', 'none', 'tied', 'evil', 'dome','mode','live',
   'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
   'flow', 'neon']
-hash = Hash.new
+hash = {}
 
 array.each do |element|
   key = element.chars.sort.join
-    if hash.has_key?(key)
-  hash[key].push(element)
-    else
-  hash[key] = [element]
+  if hash.has_key?(key)
+    hash[key].push(element)
+  else
+    hash[key] = [element]
   end
 end
 
--- anagram2.rb
+++ anagram2.rb
@@ -1,12 +1,12 @@
 array = ['demo', 'none', 'tied', 'evil', 'dome','mode','live',
   'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
   'flow', 'neon']
-hash = {}
+hash = Hash.new
 
 array.each do |element|
   key = element.split('').sort.join
   if hash.has_key?(key)
-    hash[key].push(element)
+    hash[key] << element
   else
     hash[key] = [element]
   end
